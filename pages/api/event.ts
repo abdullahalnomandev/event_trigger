ea6e542d81d98fb59a11c9dee0069ccdd1a6aba4
unknown <abdullahalnomanfb>
2022-11-09 16:10:10 +0600

    add

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+  if(req.body ){
+    console.log("response",req.body);
+      res.status(200).json({data: JSON.stringify(req.body)});
+
+  }
+  
 }
